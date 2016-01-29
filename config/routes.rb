aa1c23675a9fe977c897f39fa02aa3b44eedb3fe
Akhil <akhil.yadav@nestaway.com>
2016-01-29 13:18:00 +0530

    signup done

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,22 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'static_pages#home'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-    get 'help' => 'static_pages#help'
-    get 'about' => 'static_pages#about'
-    get 'contact' => 'static_pages#contact'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
-    get 'signup' => 'users#new'
+  get 'signup' => 'users#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
