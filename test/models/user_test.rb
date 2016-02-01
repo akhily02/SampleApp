42b1f85daddefdad5f2e249f8698149a60f384c9
Akhil <akhil.yadav@nestaway.com>
2016-02-01 13:34:18 +0530

    remember me

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -65,4 +65,8 @@ assert_not @user.valid?
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
 end
